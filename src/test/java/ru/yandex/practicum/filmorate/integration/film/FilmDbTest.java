9b0c1ad4b5dc9ab22a6a38c3094390f871bfdc68
SvetlanaKuznets0va <sv.kuznets0va@yandex.ru>
2023-04-01 14:06:55 +0300

    Исправления

--- src/test/java/ru/yandex/practicum/filmorate/integration/film/FilmDbTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/integration/film/FilmDbTest.java
@@ -19,13 +19,10 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @AutoConfigureTestDatabase
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class FilmDbTest {
     private final FilmDbStorage filmStorage;
-
     private final UserDbStorage userStorage;
 
-    @Order(1)
     @Test
     void addFilm() {
         Film film = new Film(1, "blablacar", "ужасы", LocalDate.of(2022, 12, 15), 120, new Mpa(1, "G"));
@@ -37,61 +34,94 @@ class FilmDbTest {
     @Order(2)
     @Test
     void updateFilm() {
+        Film film = new Film(1, "blablacar", "ужасы", LocalDate.of(2022, 12, 15), 120, new Mpa(1, "G"));
         Film updFilm = new Film(1, "blablacar", "не ужасы", LocalDate.of(2022, 12, 15), 120, new Mpa(1, "G"));
+        filmStorage.addFilm(film);
         filmStorage.updateFilm(updFilm);
         Film filmCheck = filmStorage.findFilmById(1);
         assertTrue(filmCheck.getDescription().equals("не ужасы"));
     }
 
-    @Order(3)
     @Test
     void findFilmById() {
+        Film film = new Film(1, "blablacar", "ужасы", LocalDate.of(2022, 12, 15), 120, new Mpa(1, "G"));
+        filmStorage.addFilm(film);
         Film filmCheck = filmStorage.findFilmById(1);
         assertTrue(filmCheck.getId() == 1);
     }
 
-    @Order(4)
     @Test
     void getAllFilms() {
+        Film film1 = new Film(1, "blablacar", "ужасы", LocalDate.of(2022, 12, 15), 120, new Mpa(1, "G"));
         Film film2 = new Film(1, "bla part 2", "ужасы", LocalDate.of(2022, 12, 27), 120, new Mpa(1, "G"));
+        filmStorage.addFilm(film1);
         filmStorage.addFilm(film2);
         List<Film> films = filmStorage.getAllFilms();
         assertTrue(films.size() == 2);
     }
 
-    @Order(5)
     @Test
     void addLike() {
+        Film film1 = new Film(1, "blablacar", "ужасы", LocalDate.of(2022, 12, 15), 120, new Mpa(1, "G"));
+        Film film2 = new Film(1, "bla part 2", "ужасы", LocalDate.of(2022, 12, 27), 120, new Mpa(1, "G"));
         User user = new User(1, "", "goshan", "Григорий Петров", LocalDate.of(2000, 05, 25));
+        filmStorage.addFilm(film1);
+        filmStorage.addFilm(film2);
         userStorage.addUser(user);
         filmStorage.addLike(2, 1);
         List<Film> films = filmStorage.getTopFilms(1);
         assertTrue(films.get(0).getId() == 2);
     }
 
-    @Order(6)
     @Test
     void getTopFilms() {
+        Film film1 = new Film(1, "blablacar", "ужасы", LocalDate.of(2022, 12, 15), 120, new Mpa(1, "G"));
+        Film film2 = new Film(1, "bla part 2", "ужасы", LocalDate.of(2022, 12, 27), 120, new Mpa(1, "G"));
+        User user1 = new User(1, "", "goshan", "Григорий Петров", LocalDate.of(2000, 05, 25));
         User user2 = new User(1, "zina@mail.ru", "zina", "Зина Сидорова", LocalDate.of(2000, 05, 25));
+        filmStorage.addFilm(film1);
+        filmStorage.addFilm(film2);
+        userStorage.addUser(user1);
         userStorage.addUser(user2);
         filmStorage.addLike(2, 2);
         filmStorage.addLike(1, 2);
+        filmStorage.addLike(2, 1);
         List<Film> films = filmStorage.getTopFilms(5);
         assertTrue(films.size() == 2);
         assertTrue(films.get(0).getId() == 2);
     }
 
-    @Order(7)
     @Test
     void getFilmsByCount() {
+        Film film1 = new Film(1, "blablacar", "ужасы", LocalDate.of(2022, 12, 15), 120, new Mpa(1, "G"));
+        Film film2 = new Film(1, "bla part 2", "ужасы", LocalDate.of(2022, 12, 27), 120, new Mpa(1, "G"));
+        User user1 = new User(1, "", "goshan", "Григорий Петров", LocalDate.of(2000, 05, 25));
+        User user2 = new User(1, "zina@mail.ru", "zina", "Зина Сидорова", LocalDate.of(2000, 05, 25));
+        filmStorage.addFilm(film1);
+        filmStorage.addFilm(film2);
+        userStorage.addUser(user1);
+        userStorage.addUser(user2);
+        filmStorage.addLike(2, 2);
+        filmStorage.addLike(1, 2);
+        filmStorage.addLike(2, 1);
         List<Film> films = filmStorage.getFilmsByCount(1);
         assertTrue(films.size() == 1);
         assertTrue(films.get(0).getId() == 1);
     }
 
-    @Order(8)
     @Test
     void deleteLike() {
+        Film film1 = new Film(1, "blablacar", "ужасы", LocalDate.of(2022, 12, 15), 120, new Mpa(1, "G"));
+        Film film2 = new Film(1, "bla part 2", "ужасы", LocalDate.of(2022, 12, 27), 120, new Mpa(1, "G"));
+        User user1 = new User(1, "", "goshan", "Григорий Петров", LocalDate.of(2000, 05, 25));
+        User user2 = new User(1, "zina@mail.ru", "zina", "Зина Сидорова", LocalDate.of(2000, 05, 25));
+        filmStorage.addFilm(film1);
+        filmStorage.addFilm(film2);
+        userStorage.addUser(user1);
+        userStorage.addUser(user2);
+        filmStorage.addLike(2, 2);
+        filmStorage.addLike(1, 2);
+        filmStorage.addLike(2, 1);
         filmStorage.deleteLike(2, 1);
         filmStorage.deleteLike(2, 2);
         filmStorage.deleteLike(1, 2);
@@ -99,11 +129,12 @@ class FilmDbTest {
         assertTrue(films.isEmpty());
     }
 
-    @Order(9)
     @Test
     void deleteFilmById() {
+        Film film = new Film(1, "blablacar", "ужасы", LocalDate.of(2022, 12, 15), 120, new Mpa(1, "G"));
+        filmStorage.addFilm(film);
         filmStorage.deleteFilmById(1);
         List<Film> films = filmStorage.getAllFilms();
-        assertTrue(films.size() == 1);
+        assertTrue(films.isEmpty());
     }
 }
--- src/test/java/ru/yandex/practicum/filmorate/integration/user/UserDbTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/integration/user/UserDbTest.java
@@ -1,10 +1,7 @@
 package ru.yandex.practicum.filmorate.integration.user;
 
 import lombok.RequiredArgsConstructor;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,16 +11,15 @@ import ru.yandex.practicum.filmorate.storage.user.UserDbStorage;
 import java.time.LocalDate;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 @AutoConfigureTestDatabase
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class UserDbTest {
     private final UserDbStorage userStorage;
 
-    @Order(1)
     @Test
     void addUser() {
         User user = new User(1, "gosha@mail.ru", "goshan", "Григорий Петров", LocalDate.of(2000, 05, 25));
@@ -32,34 +28,40 @@ class UserDbTest {
         assertTrue(checkUser.getId() == 1);
     }
 
-    @Order(2)
     @Test
     void updateUser() {
+        User user = new User(1, "gosha@mail.ru", "goshan", "Григорий Петров", LocalDate.of(2000, 05, 25));
+        userStorage.addUser(user);
         User updUser = new User(1, "gosha@mail.ru", "goshan", "Георгий Петров", LocalDate.of(2000, 05, 25));
         userStorage.updateUser(updUser);
         User checkUser = userStorage.findUserById(1);
         assertTrue(checkUser.getName().equals("Георгий Петров"));
     }
 
-    @Order(3)
     @Test
     void findUserById() {
+        User user = new User(1, "gosha@mail.ru", "goshan", "Григорий Петров", LocalDate.of(2000, 05, 25));
+        userStorage.addUser(user);
         User checkUser = userStorage.findUserById(1);
         assertTrue(checkUser.getId() == 1);
     }
 
-    @Order(3)
     @Test
     void getAllUsers() {
+        User user1 = new User(1, "gosha@mail.ru", "goshan", "Григорий Петров", LocalDate.of(2000, 05, 25));
         User user2 = new User(1, "gosha@mail.ru", "goshan", "Григорий Петров", LocalDate.of(2000, 05, 25));
+        userStorage.addUser(user1);
         userStorage.addUser(user2);
         List<User> users = userStorage.getAllUsers();
         assertTrue(users.size() == 2);
     }
 
-    @Order(4)
     @Test
     void addFriend() {
+        User user1 = new User(1, "gosha@mail.ru", "goshan", "Григорий Петров", LocalDate.of(2000, 05, 25));
+        User user2 = new User(1, "gosha@mail.ru", "goshan", "Григорий Петров", LocalDate.of(2000, 05, 25));
+        userStorage.addUser(user1);
+        userStorage.addUser(user2);
         userStorage.addFriend(1, 2);
         User checkUser1 = userStorage.findUserById(1);
         User checkUser2 = userStorage.findUserById(2);
@@ -67,29 +69,36 @@ class UserDbTest {
         assertFalse(checkUser2.getFriends().contains(1L));
     }
 
-    @Order(5)
     @Test
     void getListFriendsUser() {
+        User user1 = new User(1, "gosha@mail.ru", "goshan", "Григорий Петров", LocalDate.of(2000, 05, 25));
+        User user2 = new User(1, "gosha@mail.ru", "goshan", "Григорий Петров", LocalDate.of(2000, 05, 25));
+        userStorage.addUser(user1);
+        userStorage.addUser(user2);
         userStorage.addFriend(1, 2);
         List<User> friends = userStorage.getListFriendsUser(1);
         assertTrue(friends.size() == 1);
         assertTrue(friends.get(0).getId() == 2L);
     }
 
-    @Order(6)
     @Test
     void deleteFriend() {
+        User user1 = new User(1, "gosha@mail.ru", "goshan", "Григорий Петров", LocalDate.of(2000, 05, 25));
+        User user2 = new User(1, "gosha@mail.ru", "goshan", "Григорий Петров", LocalDate.of(2000, 05, 25));
+        userStorage.addUser(user1);
+        userStorage.addUser(user2);
         userStorage.addFriend(1, 2);
         userStorage.deleteFriend(1, 2);
         User checkUser1 = userStorage.findUserById(1);
         assertFalse(checkUser1.getFriends().contains(2L));
     }
 
-    @Order(7)
     @Test
     void deleteUserById() {
+        User user1 = new User(1, "gosha@mail.ru", "goshan", "Григорий Петров", LocalDate.of(2000, 05, 25));
+        userStorage.addUser(user1);
         userStorage.deleteUserById(1);
         List<User> users = userStorage.getAllUsers();
-        assertTrue(users.size() == 1);
+        assertTrue(users.isEmpty());
     }
 }
