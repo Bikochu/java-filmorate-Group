d7b23e1e93433c2c97e8cd2aab01cb034b72ad95
Иван Картавый <kartavey@gmail.com>
2023-04-12 13:01:25 +0300

    Добавил Функциональность «Общие фильмы»
    Добавил Функциональность «Общие фильмы»

--- src/test/java/ru/yandex/practicum/filmorate/integration/film/FilmDbTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/integration/film/FilmDbTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Mpa;
 import ru.yandex.practicum.filmorate.model.User;
@@ -21,10 +22,12 @@ import static org.junit.jupiter.api.Assertions.*;
 @AutoConfigureTestDatabase
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 class FilmDbTest {
+
     private final FilmDbStorage filmStorage;
     private final UserDbStorage userStorage;
 
     @Test
+    @DirtiesContext
     void addFilm() {
         Film film = new Film(1, "blablacar1", "ужасы", LocalDate.of(2022, 12, 15), 120, new Mpa(1, "G"), 0);
         filmStorage.addFilm(film);
@@ -33,6 +36,7 @@ class FilmDbTest {
     }
 
     @Test
+    @DirtiesContext
     void updateFilm() {
         Film film = new Film(1, "blablacar2", "ужасы", LocalDate.of(2022, 12, 15), 120, new Mpa(1, "G"), 0);
         Film updFilm = new Film(1, "blablacar2", "не ужасы", LocalDate.of(2022, 12, 15), 120, new Mpa(1, "G"), 0);
@@ -49,6 +53,7 @@ class FilmDbTest {
     }
 
     @Test
+    @DirtiesContext
     void deleteFilmById() {
         Film film = new Film(1, "blablacar3", "ужасы", LocalDate.of(2022, 12, 15), 120, new Mpa(1, "G"), 0);
         filmStorage.addFilm(film);
@@ -59,6 +64,7 @@ class FilmDbTest {
     }
 
     @Test
+    @DirtiesContext
     void findFilmById() {
         Film film = new Film(1, "blablacar4", "ужасы", LocalDate.of(2022, 12, 15), 120, new Mpa(1, "G"), 0);
         filmStorage.addFilm(film);
@@ -67,9 +73,10 @@ class FilmDbTest {
     }
 
     @Test
+    @DirtiesContext
     void getAllFilms() {
         Film film1 = new Film(1, "blablacar5", "ужасы", LocalDate.of(2022, 12, 15), 120, new Mpa(1, "G"), 0);
-        Film film2 = new Film(1, "blablacar6", "ужасы", LocalDate.of(2022, 12, 27), 120, new Mpa(1, "G"), 0);
+        Film film2 = new Film(2, "blablacar6", "ужасы", LocalDate.of(2022, 12, 27), 120, new Mpa(1, "G"), 0);
         List<Film> filmsCheckBefore = filmStorage.getAllFilms();
         filmStorage.addFilm(film1);
         filmStorage.addFilm(film2);
@@ -79,24 +86,26 @@ class FilmDbTest {
     }
 
     @Test
+    @DirtiesContext
     void addLike() {
         Film film1 = new Film(1, "blablacar7", "ужасы", LocalDate.of(2022, 12, 15), 120, new Mpa(1, "G"), 0);
-        Film film2 = new Film(1, "blablacar8", "ужасы", LocalDate.of(2022, 12, 27), 120, new Mpa(1, "G"), 0);
+        Film film2 = new Film(2, "blablacar8", "ужасы", LocalDate.of(2022, 12, 27), 120, new Mpa(1, "G"), 0);
         User user = new User(1, "", "goshan", "Григорий Петров", LocalDate.of(2000, 5, 25));
         filmStorage.addFilm(film1);
         filmStorage.addFilm(film2);
         userStorage.addUser(user);
-        filmStorage.addLike(2, 1);
+        filmStorage.addLike(film2.getId(), 1);
         List<Film> films = filmStorage.getTopFilms(1);
         assertEquals(2, films.get(0).getId());
     }
 
     @Test
+    @DirtiesContext
     void deleteLike() {
         Film film1 = new Film(1, "blablacar9", "ужасы", LocalDate.of(2022, 12, 15), 120, new Mpa(1, "G"), 0);
-        Film film2 = new Film(1, "blablacar10", "ужасы", LocalDate.of(2022, 12, 27), 120, new Mpa(1, "G"), 0);
+        Film film2 = new Film(2, "blablacar10", "ужасы", LocalDate.of(2022, 12, 27), 120, new Mpa(1, "G"), 0);
         User user1 = new User(1, "", "goshan", "Григорий Петров", LocalDate.of(2000, 5, 25));
-        User user2 = new User(1, "zina@mail.ru", "zina", "Зина Сидорова", LocalDate.of(2000, 5, 25));
+        User user2 = new User(2, "zina@mail.ru", "zina", "Зина Сидорова", LocalDate.of(2000, 5, 25));
         List<Film> filmsDrop = filmStorage.getAllFilms();
         filmsDrop.forEach(f -> filmStorage.deleteFilmById(f.getId()));
         filmStorage.addFilm(film1);
@@ -104,54 +113,54 @@ class FilmDbTest {
         userStorage.addUser(user1);
         userStorage.addUser(user2);
         List<Film> filmsUp = filmStorage.getAllFilms();
-
-        filmStorage.addLike(filmsUp.get(0).getId(), 2);
         filmStorage.addLike(filmsUp.get(0).getId(), 2);
         filmStorage.addLike(filmsUp.get(1).getId(), 1);
         filmStorage.deleteLike(filmsUp.get(1).getId(), 1);
-        filmStorage.deleteLike(filmsUp.get(1).getId(), 2);
         filmStorage.deleteLike(filmsUp.get(0).getId(), 2);
         List<Film> films = filmStorage.getTopFilms(1);
         assertTrue(films.isEmpty());
     }
 
     @Test
+    @DirtiesContext
     void getTopFilms() {
         Film film1 = new Film(1, "blablacar11", "ужасы", LocalDate.of(2022, 12, 15), 120, new Mpa(1, "G"), 0);
-        Film film2 = new Film(1, "blablacar12", "ужасы", LocalDate.of(2022, 12, 27), 120, new Mpa(1, "G"), 0);
+        Film film2 = new Film(2, "blablacar12", "ужасы", LocalDate.of(2022, 12, 27), 120, new Mpa(1, "G"), 0);
         User user1 = new User(1, "", "goshan", "Григорий Петров", LocalDate.of(2000, 5, 25));
-        User user2 = new User(1, "zina@mail.ru", "zina", "Зина Сидорова", LocalDate.of(2000, 5, 25));
+        User user2 = new User(2, "zina@mail.ru", "zina", "Зина Сидорова", LocalDate.of(2000, 5, 25));
         filmStorage.addFilm(film1);
         filmStorage.addFilm(film2);
         userStorage.addUser(user1);
         userStorage.addUser(user2);
-        filmStorage.addLike(2, 2);
-        filmStorage.addLike(1, 2);
-        filmStorage.addLike(2, 1);
+        filmStorage.addLike(film2.getId(), 2);
+        filmStorage.addLike(film1.getId(), 2);
+        filmStorage.addLike(film2.getId(), 1);
         List<Film> films = filmStorage.getTopFilms(5);
         assertEquals(2, films.size());
         assertEquals(2, films.get(0).getId());
     }
 
     @Test
+    @DirtiesContext
     void getFilmsByCount() {
         Film film1 = new Film(1, "blablacar13", "ужасы", LocalDate.of(2022, 12, 15), 120, new Mpa(1, "G"), 0);
-        Film film2 = new Film(1, "blablacar14", "ужасы", LocalDate.of(2022, 12, 27), 120, new Mpa(1, "G"), 0);
+        Film film2 = new Film(2, "blablacar14", "ужасы", LocalDate.of(2022, 12, 27), 120, new Mpa(1, "G"), 0);
         User user1 = new User(1, "", "goshan", "Григорий Петров", LocalDate.of(2000, 5, 25));
-        User user2 = new User(1, "zina@mail.ru", "zina", "Зина Сидорова", LocalDate.of(2000, 5, 25));
+        User user2 = new User(2, "zina@mail.ru", "zina", "Зина Сидорова", LocalDate.of(2000, 5, 25));
         filmStorage.addFilm(film1);
         filmStorage.addFilm(film2);
         userStorage.addUser(user1);
         userStorage.addUser(user2);
-        filmStorage.addLike(2, 2);
-        filmStorage.addLike(1, 2);
-        filmStorage.addLike(2, 1);
+        filmStorage.addLike(film2.getId(), 2);
+        filmStorage.addLike(film1.getId(), 2);
+        filmStorage.addLike(film2.getId(), 1);
         List<Film> films = filmStorage.getFilmsByCount(1);
         assertEquals(1, films.size());
         assertEquals(1, films.get(0).getId());
     }
 
     @Test
+    @DirtiesContext
     void getCommonFilms() {
         Film film1 = new Film(1, "blablacar13", "ужасы", LocalDate.of(2022, 12, 15), 120, new Mpa(1, "G"), 0);
         Film film2 = new Film(2, "blablacar14", "ужасы", LocalDate.of(2022, 12, 27), 120, new Mpa(1, "G"), 0);
@@ -163,13 +172,15 @@ class FilmDbTest {
         userStorage.addUser(user1);
         userStorage.addUser(user2);
         userStorage.addUser(user3);
-        filmStorage.addLike(1, 1);
-        filmStorage.addLike(1, 2);
-        filmStorage.addLike(2, 1);
-        filmStorage.addLike(2, 2);
-        filmStorage.addLike(2, 3);
-        List<Film> listOfFilms = filmStorage.getCommonFilms(user1.getId(), user2.getId());
-        assertEquals(2, listOfFilms.get(0).getId(), "Не соответствует.");
+        System.out.println(filmStorage.getAllFilms());
+        System.out.println(userStorage.getAllUsers());
+        filmStorage.addLike(film1.getId(), 1);
+        filmStorage.addLike(film1.getId(), 2);
+        filmStorage.addLike(film2.getId(), 1);
+        filmStorage.addLike(film2.getId(), 2);
+        filmStorage.addLike(film2.getId(), 3);
+        List<Film> listOfFilms = filmStorage.getCommonFilms(1, 2);
+        assertEquals(film2.getId(), listOfFilms.get(0).getId(), "Не соответствует.");
         assertEquals(2, listOfFilms.size(), "Не соответствует.");
     }
 }
